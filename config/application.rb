e71b2049c04763bb29b2a78a6e9613c5a43cbe94
Ivan Delaš <ivandelas@gmail.com>
2020-12-11 03:50:31 +0100

    k

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module FinanceTracker
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     # jobs backend engine
-    config.active_job.qeue_adapter = :sidekiq
+    config.active_job.queue_adapter = :sidekiq
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
